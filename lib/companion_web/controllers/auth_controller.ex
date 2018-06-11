755ff4204e0e232f71839d5ad2f619ae0b8d1d45
Rudi Giesler <rudi@praekelt.org>
2018-06-11 11:33:50 +0200

    Split out if/else into a function

--- lib/companion_web/controllers/auth_controller.ex
+++ lib/companion_web/controllers/auth_controller.ex
@@ -6,22 +6,26 @@ defmodule CompanionWeb.AuthController do
     user = %{email: auth.info.email, provider: auth.provider}
     {_, strategy_config} = Application.get_env(:ueberauth, Ueberauth)[:providers][:google]
 
-    if auth.info.urls[:website] == strategy_config[:hd] do
-      conn
-      |> put_flash(:info, "Successfully signed in!")
-      |> put_session(:user, user)
-      |> redirect(to: "/")
-    else
-      conn
-      |> put_flash(:error, "Sign in failed. Invalid domain")
-      |> redirect(to: "/")
-    end
+    conn
+    |> attempt_login(user, auth.info.urls[:website] == strategy_config[:hd])
+    |> redirect(to: "/")
   end
 
   def callback(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
     conn
     |> put_flash(:error, "Sign in failed")
     |> redirect(to: "/")
+  end
+
+  defp attempt_login(conn, user, true) do
+    conn
+    |> put_flash(:info, "Successfully signed in!")
+    |> put_session(:user, user)
+  end
+
+  defp attempt_login(conn, _user, false) do
+    conn
+    |> put_flash(:error, "Sign in failed. Invalid domain")
   end
 
   def login(conn, _params) do
