9780f26361c4576efd25c60f40811336e138a0a3
Rudi Giesler <rudi@praekelt.org>
2018-06-12 17:18:18 +0200

    Add tests for the API root

---
+++ test/controllers/apiroot_controller_test.exs
@@ -0,0 +1,25 @@
+defmodule Companion.ApiRootControllerTest do
+  use CompanionWeb.ConnCase
+
+  alias Companion.CompanionWeb.Application
+  alias Companion.Repo
+
+  @application %Application{name: "test", token: Ecto.UUID.generate()}
+
+  test "returns empty response for root", %{conn: conn} do
+    conn =
+      conn
+      |> assign(:application, @application)
+      |> get(api_root_path(conn, :index))
+
+    assert json_response(conn, :ok) == %{}
+  end
+
+  test "returns 401 response for root if not authenticated", %{conn: conn} do
+    conn =
+      conn
+      |> get(api_root_path(conn, :index))
+
+    assert response(conn, :unauthorized) == ""
+  end
+end
