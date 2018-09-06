fe0655a2d3fc28a054396f10a0b7d29202f96697
Rudi Giesler <rudi@praekelt.org>
2018-09-06 10:16:10 +0200

    Revert "Only start what we need for tests, not the whole application"
    This reverts commit 0f7fff72d9befc4673a5ef9ed980c4ee148d7e39.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,17 +1,3 @@
-# Start the repo and endpoint required for tests
-import Supervisor.Spec
-
-children = [
-  supervisor(Companion.Repo, []),
-  supervisor(CompanionWeb.Endpoint, [])
-]
-
-opts = [strategy: :one_for_one, name: Companion.Supervisor]
-Supervisor.start_link(children, opts)
-# Ensure that the application has started
-Application.ensure_all_started(:companion)
-Application.ensure_all_started(:timex)
-
 ExUnit.start()
 
 Ecto.Adapters.SQL.Sandbox.mode(Companion.Repo, :manual)
