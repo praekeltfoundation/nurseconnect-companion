055c8a614dbb1abaeeff011d3953f66f72f15e7d
Rudi Giesler <rudi@praekelt.org>
2019-02-26 14:36:32 +0200

    Update flow generator for configurable template and variables

--- lib/companion_web/controllers/flow_generator_controller.ex
+++ lib/companion_web/controllers/flow_generator_controller.ex
@@ -12,7 +12,7 @@ defmodule CompanionWeb.FlowGeneratorController do
     application = CompanionWeb.get_application!(params["application"])
 
     conn
-    |> put_resp_content_type("application/json")
+    |> put_resp_content_type("text/html")
     |> put_resp_header("content-disposition", "attachment; filename=send_#{params["date"]}.json")
     # We need to use the `jsonr` extension for the template, else Phoenix stringifys our json
     # before sending it
