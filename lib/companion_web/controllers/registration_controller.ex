f4df937aecd20b640355c7344d4158fb5a188923
Rudi Giesler <rudi@praekelt.org>
2019-06-04 17:19:51 +0200

    Formatting fix

--- lib/companion_web/controllers/registration_controller.ex
+++ lib/companion_web/controllers/registration_controller.ex
@@ -24,7 +24,9 @@ defmodule CompanionWeb.RegistrationController do
             persal(:string, "Persal code of person registered", required: false)
             sanc(:string, "SANC code of person registered", required: false)
 
-            timestamp(:string, "UTC timestamp of when the registration occurred",
+            timestamp(
+              :string,
+              "UTC timestamp of when the registration occurred",
               format: :utc_datetime
             )
           end
