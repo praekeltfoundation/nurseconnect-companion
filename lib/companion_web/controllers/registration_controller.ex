9652588e0de3125f5eb11aac30f9cd5fe8a637ae
Rudi Giesler <rudi@praekelt.org>
2019-06-04 17:05:45 +0200

    Add documentation for the registration endpoint

--- lib/companion_web/controllers/registration_controller.ex
+++ lib/companion_web/controllers/registration_controller.ex
@@ -1,5 +1,6 @@
 defmodule CompanionWeb.RegistrationController do
   use CompanionWeb, :controller
+  use PhoenixSwagger
 
   alias CompanionWeb.FallbackController
 
@@ -7,6 +8,58 @@ defmodule CompanionWeb.RegistrationController do
   alias Companion.CompanionWeb.Registration
 
   action_fallback FallbackController
+
+  def swagger_definitions do
+    %{
+      RegistrationRequest:
+        swagger_schema do
+          title("RegistrationRequest")
+          description("Schema for a registration")
+
+          properties do
+            channel(:string, "Registration channel. SMS or WhatsApp")
+            facility_code(:string, "Code for facility where registration took place")
+            msisdn(:string, "MSISDN of person registered")
+            registered_by(:string, "MSISDN of person who performed the registration")
+            persal(:string, "Persal code of person registered", required: false)
+            sanc(:string, "SANC code of person registered", required: false)
+
+            timestamp(:timestamp, "UTC timestamp of when the registration occurred",
+              format: :utc_datetime
+            )
+          end
+
+          example(%{
+            channel: "WhatsApp",
+            facility_code: "123456",
+            msisdn: "+27820001001",
+            registered_by: "+27820001002",
+            timestamp: "2018-01-01T01:01:01"
+          })
+        end
+    }
+  end
+
+  swagger_path(:create) do
+    summary("Create registration")
+    description("Creates a new registration")
+    consumes("application/json")
+    produces("application/json")
+
+    parameters do
+      body(
+        :body,
+        Schema.ref(:RegistrationRequest),
+        "The details of the registration to be created"
+      )
+    end
+
+    response(
+      201,
+      "Registration created OK",
+      Schema.ref(:RegistrationRequest)
+    )
+  end
 
   def create(conn, %{}) do
     conn = conn |> fetch_query_params()
